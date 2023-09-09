51fabd50678e6da06bafea15fa4f7a0c48d5d90b
lidingo <144531836+lidingo@users.noreply.github.com>
2023-09-09 17:14:03 +0200

    app

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from wsgiref import simple_server
+
+
+def app(environ, start_response):
+    start_response("200 OK", [])
+    return [(environ.get("HTTP_X_FORWARDED_FOR") or "?").encode()]
+
+
+simple_server.ServerHandler.server_software = ""
+port = int(os.environ.get("PORT") or 8000)
+with simple_server.make_server(host="", port=port, app=app) as httpd:
+    print(f"Serving on port {port}...")
+    httpd.serve_forever()
